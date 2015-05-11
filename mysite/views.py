40c7187456daea518a951dd8c2f62d9fc4f2ad48
biwin <biwin007@gmail.com>
2015-05-11 13:37:36 +0530

    removes the default view functions

--- mysite/views.py
+++ mysite/views.py
@@ -1,7 +1,3 @@
-from django.views.generic import View
-from django.http import HttpResponse
+from django.shortcuts import render
 
-class Index(View):
-
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('django 1.7 on Openshift')
+# Create your views here.
