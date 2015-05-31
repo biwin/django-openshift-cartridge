f7eddd3b709e58d6ff1af44e4ae7ae84e52740f4
biwin <biwin007@gmail.com>
2015-06-01 01:03:22 +0530

    adds docstring to home view.

--- demo/views.py
+++ demo/views.py
@@ -6,5 +6,10 @@ from django.shortcuts import render
 
 
 def home(request):
+    """
+    renders the deployment server details on the screen.
+    :param request: The django formatted HttpRequest
+    :return: renders context c with the demo template.
+    """
     c = dict(python_version=platform.python_version(), django_version=get_version(), pip_version=pip.__version__)
     return render(request, 'demo/demo.html', c)
