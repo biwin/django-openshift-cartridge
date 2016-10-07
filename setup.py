bbf7ec8a5f12a76e6007c10344fc2242e019fa44
biwin <biwin007@gmail.com>
2016-10-07 22:37:24 +0530

    updates version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='django-on-openshift',
-      version='2.0',
+      version='3.0',
       description='Django on OpenShift',
       author='Biwin John',
       author_email='mail@biwin.in',
