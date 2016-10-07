bd41c65d6f62b6b33a32774e6b9e81ccb83fdab5
biwin <biwin007@gmail.com>
2016-10-07 22:18:01 +0530

    changes demo view  for 1.10

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,12 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from demo import views as demo_view
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', 'demo.views.home', name='home'),
+    url(r'', demo_view.home, name='home'),
 ]
-
-
-
-
